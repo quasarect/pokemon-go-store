40fa72b98830a0a5060d0047b551b1b5e437e40d
Vishal <Vishalarya2468@gmail.com>
2023-09-25 17:29:41 +0530

    Shop PG-Sharp Page Completed

--- src/pages/PGSharp/PGSharp.jsx
+++ src/pages/PGSharp/PGSharp.jsx
@@ -1,8 +1,20 @@
-import React from 'react'
+import React from 'react';
+import './PGSharp.css'
+import PGSharpCard from '../../components/PGSharpCard/PGSharpCard';
 
 const PGSharp = () => {
+  const PGCardDetails=[1,2,3,4,5,6];
   return (
-    <div>PGSharp</div>
+    <div className="pg-sharp-wrapper">
+      <div className="pg-cards">
+        {
+          PGCardDetails.map((id)=>(
+            <div className="pg-card" key={id}><PGSharpCard/></div>
+          ))
+        }
+      
+      </div>
+    </div>
   )
 }
 
